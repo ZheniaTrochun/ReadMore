48a94db91de4c4b13ba786b7a4d0626a9efd1cd1
ZheniaTrochun <zheniatrochun@gmail.com>
2018-05-04 14:49:08 +0300

    facebook login test

--- src/main/java/com/yevhenii/kpi/readmore/ReadMoreApplication.java
+++ src/main/java/com/yevhenii/kpi/readmore/ReadMoreApplication.java
@@ -2,11 +2,35 @@ package com.yevhenii.kpi.readmore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestContextListener;
+
+import java.security.Principal;
 
 @SpringBootApplication
+@EnableOAuth2Sso
+@RestController
 public class ReadMoreApplication {
+
+	@RequestMapping("/")
+	public String test() {
+		return "TEST SUCCESS";
+	}
+
+	@RequestMapping("/user")
+	public Principal getPrincipal(Principal principal) {
+		return principal;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReadMoreApplication.class, args);
+	}
+
+	@Bean
+	public RequestContextListener requestContextListener() {
+		return new RequestContextListener();
 	}
 }
--- src/main/java/com/yevhenii/kpi/readmore/security/config/SecurityConfig.java
+++ src/main/java/com/yevhenii/kpi/readmore/security/config/SecurityConfig.java
@@ -1,82 +1,84 @@
-package com.yevhenii.kpi.readmore.security.config;
-
-import com.yevhenii.kpi.readmore.security.UserDetailsServiceImpl;
-import com.yevhenii.kpi.readmore.security.filter.JwtAuthenticationFilter;
-import com.yevhenii.kpi.readmore.security.filter.JwtLoginFilter;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    private final UserDetailsServiceImpl userDetailsService;
-
-    @Autowired
-    public SecurityConfig(UserDetailsServiceImpl userDetailsService) {
-        this.userDetailsService = userDetailsService;
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.cors()
-                .and()
-                .csrf().disable()
-                .authorizeRequests()
-                .antMatchers(HttpMethod.OPTIONS).permitAll()
-                .antMatchers("/swagger-ui.html",
-                        "/swagger-ui.html/**",
-                        "/api/**",
-                        "/swagger-resources/**",
-                        "//swagger-resources/**",
-                        "/v2/**",
-                        "/webjars/**",
-                        "/null/").permitAll()
-                .antMatchers("/book/test").permitAll()
-                .antMatchers("/actuator", "/actuator/**").permitAll()
-                .antMatchers(HttpMethod.POST, "/user/login").permitAll()
-                .antMatchers(HttpMethod.POST, "/user/register").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .addFilterBefore(new JwtLoginFilter("/user/login", authenticationManager()),
-                        UsernamePasswordAuthenticationFilter.class)
-                .addFilterBefore(new JwtAuthenticationFilter(),
-                        UsernamePasswordAuthenticationFilter.class);
-    }
-
-    @Override
-    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        default account creation
-        auth.authenticationProvider(authenticationProvider())
-                .inMemoryAuthentication()
-                .withUser("admin")
-                .password(encoder().encode("admin"))
-                .roles("USER");
-    }
-
-    @Bean
-    public PasswordEncoder encoder() {
-        return new BCryptPasswordEncoder(11);
-    }
-
-    @Bean
-    public AuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-
-        authProvider.setPasswordEncoder(encoder());
-        authProvider.setUserDetailsService(userDetailsService);
-
-        return authProvider;
-    }
-}
+//package com.yevhenii.kpi.readmore.security.config;
+//
+//import com.yevhenii.kpi.readmore.security.UserDetailsServiceImpl;
+//import com.yevhenii.kpi.readmore.security.filter.JwtAuthenticationFilter;
+//import com.yevhenii.kpi.readmore.security.filter.JwtLoginFilter;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.security.authentication.AuthenticationProvider;
+//import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//    private final UserDetailsServiceImpl userDetailsService;
+//
+//    @Autowired
+//    public SecurityConfig(UserDetailsServiceImpl userDetailsService) {
+//        this.userDetailsService = userDetailsService;
+//    }
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.cors()
+//                .and()
+//                .csrf().disable()
+//                .authorizeRequests()
+//                .antMatchers(HttpMethod.OPTIONS).permitAll()
+//                .antMatchers("/swagger-ui.html",
+//                        "/swagger-ui.html/**",
+//                        "/api/**",
+//                        "/swagger-resources/**",
+//                        "//swagger-resources/**",
+//                        "/v2/**",
+//                        "/webjars/**",
+//                        "/null/").permitAll()
+//                .antMatchers("/", "/user").permitAll()
+//                .antMatchers("/book/test").permitAll()
+//                .antMatchers("/actuator", "/actuator/**").permitAll()
+//                .antMatchers(HttpMethod.POST, "/user/login").permitAll()
+//                .antMatchers(HttpMethod.POST, "/user/register").permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+//                .addFilterBefore(new JwtLoginFilter("/user/login", authenticationManager()),
+//                        UsernamePasswordAuthenticationFilter.class)
+//                .addFilterBefore(new JwtAuthenticationFilter(),
+//                        UsernamePasswordAuthenticationFilter.class);
+//    }
+//
+//    @Override
+//    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+////        default account creation
+//        auth.authenticationProvider(authenticationProvider())
+//                .inMemoryAuthentication()
+//                .withUser("admin")
+//                .password(encoder().encode("admin"))
+//                .roles("USER");
+//    }
+//
+//    @Bean
+//    public PasswordEncoder encoder() {
+//        return new BCryptPasswordEncoder(11);
+//    }
+//
+//    @Bean
+//    public AuthenticationProvider authenticationProvider() {
+//        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+//
+//        authProvider.setPasswordEncoder(encoder());
+//        authProvider.setUserDetailsService(userDetailsService);
+//
+//        return authProvider;
+//    }
+//}
