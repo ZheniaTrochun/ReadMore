3ed0c3d94d827075fabf7a5ea3721c547aed6ef1
ZheniaTrochun <zheniatrochun@gmail.com>
2018-05-06 18:51:49 +0300

    test fb login

--- src/main/java/com/yevhenii/kpi/readmore/ReadMoreApplication.java
+++ src/main/java/com/yevhenii/kpi/readmore/ReadMoreApplication.java
@@ -1,19 +1,41 @@
 package com.yevhenii.kpi.readmore;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.RequestContextListener;
+import org.springframework.web.filter.CompositeFilter;
 
+import javax.servlet.Filter;
 import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
-@EnableOAuth2Sso
+@EnableOAuth2Client
 @RestController
-public class ReadMoreApplication {
+public class ReadMoreApplication extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	OAuth2ClientContext clientContext;
 
 	@RequestMapping("/")
 	public String test() {
@@ -32,5 +54,61 @@ public class ReadMoreApplication {
 	@Bean
 	public RequestContextListener requestContextListener() {
 		return new RequestContextListener();
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.antMatcher("/**")
+				.addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class)
+				.authorizeRequests()
+				.antMatchers("/", "/connect**", "/webjars/**")
+				.permitAll()
+				.anyRequest()
+				.authenticated()
+				.and()
+				.logout()
+				.logoutSuccessUrl("/").permitAll().and().csrf()
+				.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+	}
+
+	private Filter ssoFilter() {
+
+		CompositeFilter filter = new CompositeFilter();
+		List<Filter> filters = new ArrayList<>();
+
+		OAuth2ClientAuthenticationProcessingFilter facebookFilter = new OAuth2ClientAuthenticationProcessingFilter(
+				"/connect/facebook");
+		OAuth2RestTemplate facebookTemplate = new OAuth2RestTemplate(facebook(), clientContext);
+		facebookFilter.setRestTemplate(facebookTemplate);
+		UserInfoTokenServices tokenServices = new UserInfoTokenServices(facebookResource().getUserInfoUri(),
+				facebook().getClientId());
+		tokenServices.setRestTemplate(facebookTemplate);
+		facebookFilter.setTokenServices(tokenServices);
+
+		filters.add(facebookFilter);
+
+		filter.setFilters(filters);
+
+		return filter;
+	}
+
+	@Bean
+	public FilterRegistrationBean oauth2ClientFilterRegistration(OAuth2ClientContextFilter filter) {
+		FilterRegistrationBean registration = new FilterRegistrationBean();
+		registration.setFilter(filter);
+		registration.setOrder(-100);
+		return registration;
+	}
+
+	@Bean
+	@ConfigurationProperties("facebook.client")
+	public AuthorizationCodeResourceDetails facebook() {
+		return new AuthorizationCodeResourceDetails();
+	}
+
+	@Bean
+	@ConfigurationProperties("facebook.resource")
+	public ResourceServerProperties facebookResource() {
+		return new ResourceServerProperties();
 	}
 }
--- src/main/java/com/yevhenii/kpi/readmore/security/TokenAuthenticationService.java
+++ src/main/java/com/yevhenii/kpi/readmore/security/TokenAuthenticationService.java
@@ -1,72 +1,72 @@
-package com.yevhenii.kpi.readmore.security;
-
-import com.yevhenii.kpi.readmore.utils.properties.AppPropertyHolder;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
-import org.springframework.context.annotation.Scope;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.stereotype.Component;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.util.Collections;
-import java.util.Date;
-import java.util.Objects;
-
-@Component
-@Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
-public class TokenAuthenticationService {
-
-    private final long expiration;
-    private final String secret;
-    private final String prefix;
-    private final String header;
-
-    private static TokenAuthenticationService instance;
-
-    @Autowired
-    public TokenAuthenticationService(AppPropertyHolder propertyHolder) {
-        this.expiration = propertyHolder.getSecurity().getExpiration();
-        this.secret = propertyHolder.getSecurity().getSecret();
-        this.prefix = propertyHolder.getSecurity().getPrefix();
-        this.header = propertyHolder.getSecurity().getHeader();
-        instance = this;
-    }
-
-    public static TokenAuthenticationService getInstance() {
-
-        return instance;
-    }
-
-    public void addAuthentication(HttpServletResponse response, String username) {
-        String jwt = Jwts.builder()
-                .setSubject(username)
-                .setExpiration(new Date(System.currentTimeMillis() + expiration))
-                .signWith(SignatureAlgorithm.HS512, secret)
-                .compact();
-
-        response.addHeader(header, String.format("%s %s", prefix, jwt));
-    }
-
-    public Authentication getAuthentication(HttpServletRequest request) {
-        String token = request.getHeader(header);
-
-        if (Objects.isNull(token))
-            return null;
-
-        String username = Jwts.parser()
-                .setSigningKey(secret)
-                .parseClaimsJws(token.replace(prefix, ""))
-                .getBody()
-                .getSubject();
-
-        request.setAttribute("user", username);
-
-        return username != null ?
-                new UsernamePasswordAuthenticationToken(username, null, Collections.emptyList()) :
-                null;
-    }
-}
+//package com.yevhenii.kpi.readmore.security;
+//
+//import com.yevhenii.kpi.readmore.utils.properties.AppPropertyHolder;
+//import io.jsonwebtoken.Jwts;
+//import io.jsonwebtoken.SignatureAlgorithm;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+//import org.springframework.context.annotation.Scope;
+//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+//import org.springframework.security.core.Authentication;
+//import org.springframework.stereotype.Component;
+//
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.util.Collections;
+//import java.util.Date;
+//import java.util.Objects;
+//
+//@Component
+//@Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
+//public class TokenAuthenticationService {
+//
+//    private final long expiration;
+//    private final String secret;
+//    private final String prefix;
+//    private final String header;
+//
+//    private static TokenAuthenticationService instance;
+//
+//    @Autowired
+//    public TokenAuthenticationService(AppPropertyHolder propertyHolder) {
+//        this.expiration = propertyHolder.getSecurity().getExpiration();
+//        this.secret = propertyHolder.getSecurity().getSecret();
+//        this.prefix = propertyHolder.getSecurity().getPrefix();
+//        this.header = propertyHolder.getSecurity().getHeader();
+//        instance = this;
+//    }
+//
+//    public static TokenAuthenticationService getInstance() {
+//
+//        return instance;
+//    }
+//
+//    public void addAuthentication(HttpServletResponse response, String username) {
+//        String jwt = Jwts.builder()
+//                .setSubject(username)
+//                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+//                .signWith(SignatureAlgorithm.HS512, secret)
+//                .compact();
+//
+//        response.addHeader(header, String.format("%s %s", prefix, jwt));
+//    }
+//
+//    public Authentication getAuthentication(HttpServletRequest request) {
+//        String token = request.getHeader(header);
+//
+//        if (Objects.isNull(token))
+//            return null;
+//
+//        String username = Jwts.parser()
+//                .setSigningKey(secret)
+//                .parseClaimsJws(token.replace(prefix, ""))
+//                .getBody()
+//                .getSubject();
+//
+//        request.setAttribute("user", username);
+//
+//        return username != null ?
+//                new UsernamePasswordAuthenticationToken(username, null, Collections.emptyList()) :
+//                null;
+//    }
+//}
